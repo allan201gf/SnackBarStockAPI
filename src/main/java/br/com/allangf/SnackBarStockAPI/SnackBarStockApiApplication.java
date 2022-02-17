c8fa948e85f9d0acb31fd29f4592b049514b07b7
allan201gf <allan201gf@gmail.com>
2022-02-17 18:24:27 -0300

    Initial commit

---
+++ src/main/java/br/com/allangf/SnackBarStockAPI/SnackBarStockApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.allangf.SnackBarStockAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackBarStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackBarStockApiApplication.class, args);
+	}
+
+}
