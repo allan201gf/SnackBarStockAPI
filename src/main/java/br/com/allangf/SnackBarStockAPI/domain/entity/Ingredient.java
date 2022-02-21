2944fc744ae058a579c902164df486d9d56c8d8f
allan201gf <allan201gf@gmail.com>
2022-02-21 16:05:40 -0300

    Fixed Ingredient

--- src/main/java/br/com/allangf/SnackBarStockAPI/domain/entity/Ingredient.java
+++ src/main/java/br/com/allangf/SnackBarStockAPI/domain/entity/Ingredient.java
@@ -17,7 +17,7 @@ import javax.persistence.*;
 public class Ingredient {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int ingredientId;
 
     @Column(name = "name")
